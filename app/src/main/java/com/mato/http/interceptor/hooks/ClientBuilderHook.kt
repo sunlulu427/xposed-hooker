7acea81a002eff4ac1e815229363832a0af30ee1
sunlulu.tomato <137380027@qq.com>
2024-10-19 23:53:48 +0800

    fix make new response body error

--- app/src/main/java/com/mato/http/interceptor/hooks/ClientBuilderHook.kt
+++ app/src/main/java/com/mato/http/interceptor/hooks/ClientBuilderHook.kt
@@ -31,15 +31,12 @@ class ClientBuilderHook(
             beforeMethodHooker(false) {
                 val interceptorClazz = classLoader.loadClass("okhttp3.Interceptor")
                     ?: return@beforeMethodHooker
-                XposedBridge.log("interceptor: $interceptorClazz")
                 val method = builderClass.getMethod("addInterceptor", interceptorClazz)
-                XposedBridge.log("method: $method")
                 val interceptor = Proxy.newProxyInstance(
                     classLoader,
                     arrayOf(interceptorClazz),
                     this
                 )
-                XposedBridge.log("create interceptor result")
                 method.invoke(it.thisObject, interceptor)
             }
         )
@@ -50,31 +47,25 @@ class ClientBuilderHook(
     }
 
     override fun invoke(proxy: Any?, method: Method?, args: Array<out Any>?): Any {
-        XposedBridge.log("proxy invoke: $method")
         return when (method?.name) {
-            "interceptor" -> {
+            "intercept" -> {
+                // return response
                 onInterceptorIntercept(args!!.first())
             }
+
             else -> null
         } ?: Any()
     }
 
     private fun onInterceptorIntercept(chain: Any): Any? {
         val request = XposedHelpers.callMethod(chain, "request")
-        XposedBridge.log("request: $request")
         val response = XposedHelpers.callMethod(chain, "proceed", request)
         val body = XposedHelpers.callMethod(response, "body")
 
-        if (body != null) {
-            XposedBridge.log("got response body: $body")
-        }
         if (body != null && body::class.java.name.startsWith("okhttp3.")) {
             val contentType = XposedHelpers.callMethod(body, "contentType")
             if (contentType?.toString()?.contains("application/json") == true) {
-                val bodyString = XposedHelpers.callMethod(body, "string") as? String
-                    ?: return null
-                XposedBridge.log("response: $bodyString")
-                return runCatching { makeNewResponse(contentType, bodyString, response, body) }
+                return runCatching { makeNewResponse(contentType, response, body) }
                     .onFailure(XposedBridge::log)
                     .getOrDefault(response)
             }
@@ -84,12 +75,13 @@ class ClientBuilderHook(
 
     private fun makeNewResponse(
         contentType: Any,
-        bodyString: String,
         response: Any,
         body: Any
     ): Any {
+        val bodyString = XposedHelpers.callMethod(body, "string")
+            ?: return response
         val newBody = XposedHelpers.callMethod(
-            body::class.java,
+            body,
             "create",
             contentType,
             bodyString
